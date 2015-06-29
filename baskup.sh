9b29efef1afa06540b2469ef996ab75972149e30
pazpi <pasettodavide@gmail.com>
2015-06-29 20:10:26 +0200

    Create remote folder for current backup

--- baskup.sh
+++ baskup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Qui ci sono le variabili da modificare per il vostro utilizzo
 CWD=$(/usr/bin/pwd)
 EXCLUDE_FILTER=$CWD/exlude-path.txt
 HN=$(/bin/hostname)
@@ -8,9 +9,12 @@ LOCAL_FOLDER=$(/)
 REMOTE_FOLDER=$(/media/1TORRHDD)
 MAIL=pasettodavide@gmail.com
 TESTO_MAIL=$CWD/mail.txt
-
+DATE="date +'%B'"
+# Fine! Da qui in poi è a vostro rischio 
 
 echo "Copia in corso di $(LOCAL_FOLDER) dentro la cartella remota $(REMOTE/FOLDER)"
+# Make remote folder
+ssh $SSH_NAME "mkdir $REMOTE_FOLDER/backup-$HN-$DATE"
 
 rsync -avx --timeout --exlude-from=$EXCLUDE_FILTER $(LOCAL_FOLDER) -e ssh $SSH_NAME:$REMOTE_FOLDER
 
@@ -21,3 +25,5 @@ ssh $SSH_NAME "du -sh $REMOTE_FOLDER | tail -n 1" > $TESTO_MAIL
 # video di Morrolinux su come impostare un mail server che ci farà da supporto
 
 mail -s "backup-completato" $MAIL $TESTO_MAIL
+
+rm -f $TEST_MAIL
