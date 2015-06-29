3e8af9d0beb0b64fa3824ad9dc65f1fc33c00c78
pazpi <pasettodavide@gmail.com>
2015-06-29 19:46:19 +0200

    Calc total space by current backup

--- baskup.sh
+++ baskup.sh
@@ -3,7 +3,7 @@
 CWD=$(/usr/bin/pwd)
 EXCLUDE_FILTER=$CWD/exlude-path.txt
 HN=$(/bin/hostname)
-SSH_USER=pi
+SSH_NAME=pi
 LOCAL_FOLDER=$(/)
 REMOTE_FOLDER=$(/media/1TORRHDD)
 MAIL=pasettodavide@gmail.com
@@ -12,7 +12,9 @@ TESTO_MAIL=$CWD/mail.txt
 
 echo "Copia in corso di $(LOCAL_FOLDER) dentro la cartella remota $(REMOTE/FOLDER)"
 
-rsync -avx --timeout --exlude-from=$EXCLUDE_FILTER $(LOCAL_FOLDER) -e ssh pi:$REMOTE_FOLDER
+rsync -avx --timeout --exlude-from=$EXCLUDE_FILTER $(LOCAL_FOLDER) -e ssh $SSH_NAME:$REMOTE_FOLDER
+
+ssh $SSH_NAME "du -sh $REMOTE_FOLDER | tail -n 1" > $TESTO_MAIL
 
 # Invia una mail per informare il corretto backup
 # https://www.youtube.com/watch?v=A_jehHsTzvE
