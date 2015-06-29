0fa23da77271f682a295e32cd34fc061ced08be1
pazpi <pasettodavide@gmail.com>
2015-06-29 19:23:04 +0200

    Add some basic command and variable

--- baskup.sh
+++ baskup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 CWD=$(/usr/bin/pwd)
-EXCLUDE=$CWD/exlude-path.txt
+EXCLUDE_FILTER=$CWD/exlude-path.txt
 HN=$(/bin/hostname)
+SSH_USER=pi
+LOCAL_FOLDER=$(/)
+REMOTE_FOLDER=$(/media/1TORRHDD)
 
+echo "Copia in corso di $(LOCAL_FOLDER) dentro la cartella remota $(REMOTE/FOLDER)"
+
+rsync -avx --timeout --exlude-from=$EXCLUDE_FILTER $(LOCAL_FOLDER) -e ssh pi:$REMOTE_FOLDER
 
